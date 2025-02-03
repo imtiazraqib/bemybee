28857dea5c1ab39b7abe1eb62fe47bfc8efbb76a
imtiazraqib <imtiazud@ualberta.ca>
2025-02-03 00:04:11 -0700

    moved useSearchParams within suspense fallback

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,31 @@
 "use client";
+
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
 import ValentineCard from "./components/ValentineCard";
 import PersonalizeButton from "./components/PersonalizeButton";
-import { Suspense } from "react";
 
-export default function Home() {
+function HomeContent() {
   const searchParams = useSearchParams();
   const nameFromParams = searchParams.get("name");
   const personName = nameFromParams ?? "mi amor";
 
   return (
+    <>
+      <PersonalizeButton />
+      <ValentineCard personName={personName} />
+    </>
+  );
+}
+
+export default function Home() {
+  return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24 bg-pink-100">
       <Suspense fallback={<div>Loading...</div>}>
-        <PersonalizeButton />
-        <ValentineCard personName={personName} />
+        {/* The useSearchParams() call happens *inside* HomeContent, 
+            which is fully wrapped by Suspense. */}
+        <HomeContent />
       </Suspense>
     </div>
   );
